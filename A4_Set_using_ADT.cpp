f22b92f21eb06270a951c64565af38577167b006
Aakash <177605571+aakashaygonde@users.noreply.github.com>
2025-05-08 07:15:32 +0530

    Update A4_Set_using_ADT.cpp

--- A4_Set_using_ADT.cpp
+++ A4_Set_using_ADT.cpp
@@ -1,5 +1,5 @@
 /*
-A4 : 
+A4 : Use Samidha's code for this one.
         "To create ADT that implement the "set" concept. a. Add (new Element) -Place a value into the set , b. Remove (element) Remove the value c. Contains (element) Return true if element is in collection, d. Size () Return number of values in collection Iterator () Return an iterator used to loop over collection, e. Intersection of two sets , f. Union of two sets, g. Difference between two sets, h. Subset "
 */
 #include <iostream>
